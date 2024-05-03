b2f95abfb277ce26da29076e84608ce74cb70990
PhongThanh0907 <phong.thanh971997@gmail.com>
2024-05-03 15:26:22 +0700

    update code

--- src/components/WorkExperiment/index.tsx
+++ src/components/WorkExperiment/index.tsx
@@ -12,8 +12,6 @@ import Image from "next/image";
 import TextLight from "../TextLight";
 import Anpha from "../../../public/logoanpha.jpg";
 import { useTranslation } from "@/providers/TranslateProvider";
-import Header from "../Header";
-import Test from "./test";
 
 const ExperienceCard = React.memo(({ experience }: { experience: any }) => {
   return (
